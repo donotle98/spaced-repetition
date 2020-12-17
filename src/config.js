6d24c39aefb2819bc4a2566fbca312b95c857ad5
Donovan <donovanle18@gmail.com>
2020-12-17 14:20:59 -0600

    changed heroku url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
-}
+    API_ENDPOINT: "https://arcane-hollows-39259.herokuapp.com/",
+    TOKEN_KEY: "blogful-client-auth-token",
+};
