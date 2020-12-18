cfc27e1f33dde1362a46860499916f47b6dd98b8
Donovan <donovanle18@gmail.com>
2020-12-18 13:21:40 -0600

    heroku deployed

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+    Heroku_ENDPOINT: "http://localhost:8000/api",
     API_ENDPOINT: "https://arcane-hollows-39259.herokuapp.com/api",
     TOKEN_KEY: "blogful-client-auth-token",
 };
