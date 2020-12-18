e1255823b925e0e7d3680ced1c80b095d1f15e74
Donovan <donovanle18@gmail.com>
2020-12-18 15:59:25 -0600

    route changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    API_ENDPOINT: "http://localhost:8000/api/",
-    noisand_ENDPOINT:
-        "https://cors-anywhere.herokuapp.com/https://lit-fortress-70066.herokuapp.com/api/",
+    xxx_ENDPOINT: "http://localhost:8000/api/",
+    API_ENDPOINT:
+        "https://cors-anywhere.herokuapp.com/https://blooming-chamber-02747.herokuapp.com/api/",
     TOKEN_KEY: "blogful-client-auth-token",
 };
